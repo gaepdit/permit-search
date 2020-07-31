6464825b5ad291035ed040e73d6a373e0e544071
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-07-31 09:56:21 -0400

    Parameterize query in permit page
    closes #10

--- permitsearch.gaepd.org/permit.aspx.vb
+++ permitsearch.gaepd.org/permit.aspx.vb
@@ -1,113 +1,73 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Linq
+Imports System.Net
 
 Public Class permit
     Inherits Page
 
     Public ReadOnly Property RaygunApiKey = CType(ConfigurationManager.GetSection("RaygunSettings"), Mindscape.Raygun4Net.RaygunSettings).ApiKey
 
-    Dim file As String
-    Dim MyData As Byte()
-
-    Protected Sub lbtReturn_Click(ByVal sender As Object, ByVal e As EventArgs) Handles lbtReturn.Click
-        Response.Write("<script language='javascript'> { window.close();}</script>")
-    End Sub
-
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-        lbtReturn.Attributes.Add("onclick", "window.close();")
-
-        file = Request.QueryString("id")
-
-        If file = "" Then
-            NoPermit.Visible = True
+        Dim file As String = Request.QueryString("id")
+        If String.IsNullOrEmpty(file) Then
+            Response.StatusCode = HttpStatusCode.BadRequest
             Return
         End If
 
         Dim ext As String = Mid(file, 1, 3)
-        If ext = "PDF" Then
-
-            Dim sql = "SELECT pdfpermitdata FROM apbpermits WHERE strFILENAME = '" & Mid(file, 5) & "' "
-            Dim conn As New SqlConnection(strDBConnection)
-            Dim cmd As New SqlCommand(sql, conn) With {
-                .CommandType = CommandType.Text
-            }
-
-            If conn.State <> ConnectionState.Open Then
-                conn.Open()
-            End If
-
-            Dim dr As SqlDataReader = cmd.ExecuteReader()
-            Dim recExist As Boolean = dr.Read
-
-            If recExist Then
-                NoPermit.Visible = False
-                Me.Title = "GA AIR - File " & file
-                MyData = CType(dr.Item("pdfpermitdata"), Byte())
-                Response.Clear()
-                Response.ClearHeaders()
-                Response.ClearContent()
-                Response.Buffer = True
-                Response.ContentType = "application/pdf"
-                Response.AddHeader("content-disposition", "inline;filename=" & file & ".pdf")
-                Response.Charset = ""
-                Response.Cache.SetCacheability(HttpCacheability.NoCache)
-                Response.BinaryWrite(MyData)
-                Response.Flush()
-                Response.End()
-            Else
-                NoPermit.Visible = True
-            End If
-
-            If conn.State = ConnectionState.Open Then
-                conn.Close()
-            Else
-                'conn.Open()
-            End If
-
-        ElseIf ext = "DOC" Then
-
-            Dim sql = "SELECT docpermitdata FROM apbpermits WHERE strFILENAME = '" & Mid(file, 5) & "' "
-            Dim conn As New SqlConnection(strDBConnection)
-            Dim cmd As New SqlCommand(sql, conn) With {
-                .CommandType = CommandType.Text
-            }
-
-            If conn.State <> ConnectionState.Open Then
-                conn.Open()
-            End If
-
-            Dim dr As SqlDataReader = cmd.ExecuteReader()
-            Dim recExist As Boolean = dr.Read
-            If recExist Then
-                NoPermit.Visible = False
-                Me.Title = "GA AIR - File " & file
-                MyData = CType(dr.Item("docpermitdata"), Byte())
-                Response.Clear()
-                Response.ClearHeaders()
-                Response.ClearContent()
-                Response.Buffer = True
-                Response.ContentType = "application/msword"
-                Response.AddHeader("content-disposition", "inline;filename=" & file & ".doc")
-                Response.Charset = ""
-                Response.Cache.SetCacheability(HttpCacheability.NoCache)
-                Response.BinaryWrite(MyData)
-                Response.Flush()
-                Response.End()
-            Else
-                NoPermit.Visible = True
-            End If
-
-            If conn.State = ConnectionState.Open Then
-                conn.Close()
-            Else
-                'conn.Open()
-            End If
-
-
-
-        Else
-            NoPermit.Visible = True
+        If Not {"PDF", "DOC"}.Contains(ext) Then
+            Response.StatusCode = HttpStatusCode.NotFound
+            Return
         End If
 
+        Dim filename As String = Mid(file, 5)
+        If String.IsNullOrEmpty(filename) Then
+            Response.StatusCode = HttpStatusCode.NotFound
+            Return
+        End If
+
+        Dim sql As String
+        If ext = "PDF" Then
+            sql = "SELECT pdfpermitdata FROM apbpermits WHERE strFILENAME = @filename "
+        Else
+            sql = "SELECT docpermitdata FROM apbpermits WHERE strFILENAME = @filename "
+        End If
+
+        Dim MyData As Byte()
+
+        Using connection As New SqlConnection(strDBConnection)
+            Using command As New SqlCommand(sql, connection)
+                command.CommandType = CommandType.Text
+                command.Parameters.AddWithValue("@filename", filename)
+                command.Connection.Open()
+                MyData = command.ExecuteScalar()
+                command.Connection.Close()
+            End Using
+        End Using
+
+        If MyData Is Nothing Then
+            Response.StatusCode = HttpStatusCode.NotFound
+            Return
+        End If
+
+        Response.Clear()
+        Response.ClearHeaders()
+        Response.ClearContent()
+        Response.Buffer = True
+
+        If ext = "PDF" Then
+            Response.ContentType = "application/pdf"
+            Response.AddHeader("content-disposition", "inline;filename=" & file & ".pdf")
+        Else
+            Response.ContentType = "application/msword"
+            Response.AddHeader("content-disposition", "inline;filename=" & file & ".doc")
+        End If
+
+        Response.Charset = ""
+        Response.Cache.SetCacheability(HttpCacheability.Public)
+        Response.BinaryWrite(MyData)
+        Response.Flush()
+        Response.End()
     End Sub
 
 End Class
