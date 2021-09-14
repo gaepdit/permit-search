fbd6bfb188c32428932e0f8a261b26d5f03db63e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-09-14 10:32:12 -0400

    Add test site banner

--- permit-search/Main.Master.vb
+++ permit-search/Main.Master.vb
@@ -2,5 +2,6 @@
     Inherits MasterPage
 
     Public ReadOnly Property RaygunApiKey = CType(ConfigurationManager.GetSection("RaygunSettings"), Mindscape.Raygun4Net.RaygunSettings).ApiKey
+    Public ReadOnly Property CurrentEnvironment As String = ConfigurationManager.AppSettings("APP_ENVIRONMENT")
 
 End Class
