fb356e58f99b93d0bb7b5178da5a90505cea219e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-09-22 14:51:45 -0400

    Bump version: 2021.9.22

--- permit-search/My Project/AssemblyInfo.vb
+++ permit-search/My Project/AssemblyInfo.vb
@@ -30,4 +30,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2021.9.14.0")>
+<Assembly: AssemblyVersion("2021.9.22.0")>
