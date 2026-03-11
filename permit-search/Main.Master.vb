46bd61f95746b71f61f2da05dc266ffc846b21b0
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2026-03-11 10:40:11 -0400

    Update RUM config (#62)
    Remove Raygun RUM and switch Datadog RUM to async

--- permit-search/Main.Master.vb
+++ permit-search/Main.Master.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Main
     Inherits MasterPage
 
-    Public ReadOnly Property RaygunApiKey = CType(ConfigurationManager.GetSection("RaygunSettings"), Mindscape.Raygun4Net.RaygunSettings).ApiKey
     Public ReadOnly Property CurrentEnvironment As String = ConfigurationManager.AppSettings("APP_ENVIRONMENT")
 
 End Class
